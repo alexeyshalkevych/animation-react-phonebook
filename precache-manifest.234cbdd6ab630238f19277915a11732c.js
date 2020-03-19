2fa97798c6832756f7f90e5046ede045a7371490
Alexey <valnistuy@gmail.com>
2020-03-19 22:39:26 +0200

    Updates

---
+++ precache-manifest.234cbdd6ab630238f19277915a11732c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcdc60b227202389f74c2e4bec69698",
+    "url": "/animation-react-phonebook/index.html"
+  },
+  {
+    "revision": "568175f1b4a0a95ba89f",
+    "url": "/animation-react-phonebook/static/css/main.427b15a6.chunk.css"
+  },
+  {
+    "revision": "8259470962e3610376dd",
+    "url": "/animation-react-phonebook/static/js/2.7dc8867f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/animation-react-phonebook/static/js/2.7dc8867f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568175f1b4a0a95ba89f",
+    "url": "/animation-react-phonebook/static/js/main.f87ed50e.chunk.js"
+  },
+  {
+    "revision": "9382f16aa69e32f1f641",
+    "url": "/animation-react-phonebook/static/js/runtime-main.6187129e.js"
+  }
+]);
